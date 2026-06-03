f5f128f1487b04890251fb45a7dc84b6067d3442
SamuelSous0 <samuel.00000859887@unicap.br>
2026-06-03 10:46:51 +0000

    login using email field

--- avalia-filme-app/app/login/page.js
+++ avalia-filme-app/app/login/page.js
@@ -2,12 +2,11 @@
 import { useState } from "react";
 import { userLogin } from "../../services/userApi";
 import { useRouter } from "next/navigation";
-
 import styles from "./login.module.css";
 
 export default function LoginPage() {
   const router = useRouter();
-  const [form, setForm] = useState({ name: "", password: "" });
+  const [form, setForm] = useState({ email: "", password: "" }); 
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
 
@@ -16,7 +15,7 @@ export default function LoginPage() {
     setLoading(true);
     try {
       const user = await userLogin({
-        name: form.name.trim(),
+        email: form.email.trim(),  
         password: form.password.trim()
       });
       localStorage.setItem("userId", user.id);
@@ -32,12 +31,17 @@ export default function LoginPage() {
   return (
     <div className={styles.containerAutenticacao}>
       <h2>Entrar</h2>
-      <input placeholder="e-mail"
-        value={form.email}
-        onChange={e => setForm({ ...form, email: e.target.value })} />
-      <input type="password" placeholder="Senha"
+      <input
+        placeholder="E-mail"
+        value={form.email}                                          
+        onChange={e => setForm({ ...form, email: e.target.value })} 
+      />
+      <input
+        type="password"
+        placeholder="Senha"
         value={form.password}
-        onChange={e => setForm({ ...form, password: e.target.value })} />
+        onChange={e => setForm({ ...form, password: e.target.value })}
+      />
       {error && <p className={styles.erro}>{error}</p>}
       <button onClick={handleSubmit} disabled={loading}>
         {loading ? "Entrando..." : "Entrar"}
